8cd44458928eefd7f9f0068171da66dbb7e19d4c
Ye Sijun <junnplus@gmail.com>
2022-04-25 21:33:00 +0800

    integrate clangd_extensions.nvim
    Signed-off-by: Ye Sijun <junnplus@gmail.com>

---
+++ lua/nvim-lsp-setup/clangd_extensions.lua
@@ -0,0 +1,47 @@
+-- Copy from: https://github.com/p00f/clangd_extensions.nvim/blob/453b5d8fef67ec62ed184c035a374d1016a395bd/lua/clangd_extensions/init.lua
+-- MIT License, Copyright (c) 2022 Chinmay Dalal
+
+local config = require('clangd_extensions.config')
+
+local M = {}
+
+M.hint_aucmd_set_up = false
+
+local commands = [[
+if !exists(':ClangdAST')
+  function s:memuse_compl(_a,_b,_c)
+      return ['expand_preamble']
+  endfunction
+  command ClangdSetInlayHints lua require('clangd_extensions.inlay_hints').set_inlay_hints()
+  command ClangdDisableInlayHints lua require('clangd_extensions.inlay_hints').disable_inlay_hints()
+  command ClangdToggleInlayHints lua require('clangd_extensions.inlay_hints').toggle_inlay_hints()
+  command -range ClangdAST lua require('clangd_extensions.ast').display_ast(<line1>, <line2>)
+  command ClangdTypeHierarchy lua require('clangd_extensions.type_hierarchy').show_hierarchy()
+  command ClangdSymbolInfo lua require('clangd_extensions.symbol_info').show_symbol_info()
+  command -nargs=? -complete=customlist,s:memuse_compl ClangdMemoryUsage lua require('clangd_extensions.memory_usage').show_memory_usage('<args>' == 'expand_preamble')
+endif
+]]
+
+function M.setup(opts)
+    -- Set up extensions, get lspconfig opts
+    config.setup(opts)
+    -- Set up autocommands for inlay hints
+    local old_func = config.options.server.on_attach
+    config.options.server.on_attach = function(client, bufnr)
+        if old_func then
+            old_func(client, bufnr)
+        end
+        if config.options.extensions.autoSetHints then
+            require('clangd_extensions.inlay_hints').setup_autocmd()
+            require('clangd_extensions.inlay_hints').set_inlay_hints()
+        end
+        vim.cmd(commands)
+    end
+
+    -- Set up AST state stuff
+    require('clangd_extensions.ast').init()
+
+    return config.options.server
+end
+
+return M
