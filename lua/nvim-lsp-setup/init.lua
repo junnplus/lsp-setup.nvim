badddeb734882f5e77eafe25dcc8d1e349f3d072
Ye Sijun <junnplus@gmail.com>
2022-07-29 11:02:02 +0800

    setup handlers with mason-lspconfig
    Signed-off-by: Ye Sijun <junnplus@gmail.com>

--- lua/nvim-lsp-setup/init.lua
+++ lua/nvim-lsp-setup/init.lua
@@ -1,32 +1,9 @@
 local _ = require('mason-core.functional')
 local utils = require('nvim-lsp-setup.utils')
 
-local M = {}
-
-function M.setup(opts)
-    opts = vim.tbl_deep_extend('keep', opts, {
-        default_mappings = true,
-        mappings = {},
-        servers = {},
-        capabilities = vim.lsp.protocol.make_client_capabilities(),
-    })
-    local servers = opts.servers
-    local mappings = opts.mappings
-
-    setmetatable(servers, {
-        __index = function()
-            return {}
-        end,
-    })
-
-    if vim.api.nvim_get_commands({})['Mason'] == nil then
-        require('mason').setup({})
-    end
-    require('mason-lspconfig').setup({
-        ensure_installed = _.keys(servers),
-    })
-
-    for server, config in pairs(servers) do
+local function lsp_servers(opts)
+    local servers = {}
+    for server, config in pairs(opts.servers) do
         local server_name, _ = require('mason-core.package').Parse(server)
 
         config = vim.tbl_deep_extend('keep', config, {
@@ -50,16 +27,44 @@ function M.setup(opts)
         local on_attach = config.on_attach
         config.on_attach = function(client, bufnr)
             if opts.default_mappings then
-                utils.default_mappings(bufnr, mappings)
+                utils.default_mappings(bufnr, opts.mappings)
             else
-                utils.mappings(bufnr, mappings)
+                utils.mappings(bufnr, opts.mappings)
             end
 
             on_attach(client, bufnr)
         end
 
-        require('lspconfig')[server_name].setup(config)
+        servers[server_name] = config
     end
+
+    return servers
+end
+
+local M = {}
+
+function M.setup(opts)
+    opts = vim.tbl_deep_extend('keep', opts, {
+        default_mappings = true,
+        mappings = {},
+        servers = {},
+        capabilities = vim.lsp.protocol.make_client_capabilities(),
+    })
+
+    local servers = lsp_servers(opts)
+
+    if vim.api.nvim_get_commands({})['Mason'] == nil then
+        require('mason').setup()
+    end
+    require('mason-lspconfig').setup_handlers({
+        function(server_name)
+            local config = servers[server_name] or {}
+            require('lspconfig')[server_name].setup(config)
+        end
+    })
+    require('mason-lspconfig').setup({
+        ensure_installed = _.keys(opts.servers),
+    })
 end
 
 return M
