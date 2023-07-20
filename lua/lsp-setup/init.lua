47a65d5b7114d50d1dc21d394b031c95da2f9b2a
Dingcheng Yue <darwinsenior@gmail.com>
2023-07-20 09:52:44 +0100

    fix: apply cmp capability after setup

--- lua/lsp-setup/init.lua
+++ lua/lsp-setup/init.lua
@@ -19,7 +19,11 @@ local function lsp_servers(opts)
 
             local ok1, cmp = pcall(require, 'cmp_nvim_lsp')
             if ok1 then
-                config.capabilities = cmp.default_capabilities(config.capabilities)
+                config.capabilities = vim.tbl_deep_extend(
+                    'keep',
+                    cmp.default_capabilities(),
+                    opts.capabilities
+                )
             end
             local ok2, coq = pcall(require, 'coq')
             if ok2 then
